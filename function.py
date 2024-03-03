f66d9f6c53bb343bc051895a0d9df81d291620ec
Jingtao Xie <jxie275@aucklanduni.ac.nz>
2024-03-03 15:38:19 +1300

    #20240302 Basic calculator run on console

---
+++ function.py
@@ -0,0 +1,30 @@
+
+def printResult(result):
+    print(result)
+
+def plus(num1,num2):
+    sumvalue=float(num1)+float(num2)
+    return sumvalue
+
+def minus(num1,num2):
+    differencevalue=float(num1)-float(num2)
+    return differencevalue
+
+def mutiple(num1,num2):
+    mutiplevalue=float(num1)*float(num2)
+    return mutiplevalue
+
+def divisor(num1,num2):
+    if(num2!=0):
+     dividevalue=float(num1)/float(num2)
+     return dividevalue
+
+
+def ask_continue():
+        print("Continue? Y/N")
+        cancontinue = input()
+        if cancontinue == "Y":
+            return True
+        else:
+            return False
+
